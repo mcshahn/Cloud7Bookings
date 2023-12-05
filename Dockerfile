4a1590dcd708183338ea788e58b29fb7766a5812
MichelleAhn <mcshahn@gmail.com>
2023-12-05 14:07:48 -0500

    added sso

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@
 
 FROM python:3.8-slim-buster
 
+ENV GOOGLE_CLIENT_ID=318056048243-6adot3h6126usbta6r202puo7meibsng.apps.googleusercontent.com
+ENV GOOGLE_CLIENT_SECRET=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
+ENV SECRET_KEY=x1cuDp1tML0QEuLHbBbfatpVyYqDfjLuTrzqXe7Z
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
--- app.py
+++ app.py
@@ -25,6 +25,55 @@ from events.send_message import publish_message_to_sns
 
 app = FastAPI()
 
+import os
+from starlette.config import Config
+from authlib.integrations.starlette_client import OAuth
+
+# OAuth settings
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or None
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or None
+if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
+    raise BaseException('Missing env variables')
+
+# Set up oauth
+config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+starlette_config = Config(environ=config_data)
+oauth = OAuth(starlette_config)
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'},
+)
+
+
+from starlette.middleware.sessions import SessionMiddleware
+SECRET_KEY = os.environ.get('SECRET_KEY') or None
+if SECRET_KEY is None:
+    raise 'Missing SECRET_KEY'
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+
+
+from fastapi import Request
+from starlette.responses import RedirectResponse
+from authlib.integrations.starlette_client import OAuthError
+
+@app.route('/login')
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')  # This creates the url for the /auth endpoint
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.route('/auth')
+async def auth(request: Request):
+    try:
+        access_token = await oauth.google.authorize_access_token(request)
+    except OAuthError:
+        return RedirectResponse(url='/')
+    # user_data = await oauth.google.parse_id_token(request, access_token)
+    user_data = access_token["userinfo"]
+    request.session['user'] = dict(user_data)
+    return RedirectResponse(url='/')
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -62,58 +111,80 @@ bookings_resource = get_bookings_resource()
 # END TODO
 # **************************************
 
+from starlette.responses import HTMLResponse
+@app.get('/')
+def public(request: Request):
+    user = request.session.get('user')
+    if user:
+        name = user.get('name')
+        return HTMLResponse(f'<p>Hello {name}!</p><a href=/logout>Logout</a>')
+    return HTMLResponse('<p> Please log in to access the bookings microservice<p> <a href=/login>Login</a>')
 
-@app.get("/")
+
+@app.route('/logout')
+async def logout(request: Request):
+    request.session.pop('user', None)
+    return RedirectResponse(url='/')
+
+
+@app.get("/homepage")
 async def root():
     return RedirectResponse("/static/index.html")
 
 
 @app.get("/bookings", response_model=List[BookingRspModel])
-async def get_bookings():
+async def get_bookings(request: Request):
     """
     Return a list of bookings matching a query string.
 
     - **booking_id**: booking's id
    
     """
-    result = bookings_resource.get_bookings()
-    # print("in /bookings", result)
-    return result
+
+    user = request.session.get('user')
+    if user:
+        result = bookings_resource.get_bookings()
+        return result
+    return RedirectResponse(url='/')
+
 @app.get("/bookings/booking_id/{booking_id}", response_model=Union[BookingRspModel, None])
-async def get_booking_by_booking_id(booking_id: str):
+async def get_booking_by_booking_id(booking_id: str, request: Request):
     """
     Return a booking based on booking_id.
 
     - **booking_id**: booking's id
     """
-    result = None
-    
-    result = bookings_resource.get_bookings_by_booking_id(booking_id)
-    # print(result)
-    if len(result) == 1:
-        result = result[0]
-    else:
-        raise HTTPException(status_code=404, detail="Not found")
+    user = request.session.get('user')
+    if user:
+        result = bookings_resource.get_bookings_by_booking_id(booking_id)
+        # print(result)
+        if len(result) == 1:
+            result = result[0]
+        else:
+            raise HTTPException(status_code=404, detail="Not found")
 
-    return result
+        return result
+    return RedirectResponse(url='/')
 
 @app.get("/bookings/space_id/{space_id}", response_model=Union[BookingRspModel, None])
-async def get_booking_by_space_id(space_id: str):
+async def get_booking_by_space_id(space_id: str, request: Request):
     """
     Return a booking based on booking_id.
 
     - **booking_id**: booking's id
     """
-    result = None
-    
-    result = bookings_resource.get_bookings_by_space_id(space_id)
-    # print(result)
-    if len(result) == 1:
-        result = result[0]
-    else:
-        raise HTTPException(status_code=404, detail="Not found")
+    user = request.session.get('user')
+    if user:
+        result = bookings_resource.get_bookings_by_space_id(space_id)
+        # print(result)
+        if len(result) == 1:
+            result = result[0]
+        else:
+            raise HTTPException(status_code=404, detail="Not found")
 
-    return result
+        return result
+    return RedirectResponse(url='/')
+
 
 @app.post("/bookings/") #append/create a new one; if something exists-->append
 async def create_item(item: BookingRspModel):
